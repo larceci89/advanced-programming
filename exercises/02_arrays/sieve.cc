2db77b3d3020ec85d4b65489ec87120c4f868784
larceci <larceci@sissa.it>
2017-10-24 20:28:03 +0200

    sieve.cc iniziato

---
+++ exercises/02_arrays/sieve.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+	//take maximum number from input
+
+	//initialize boolean vector of length max_number
+
+	//cycle from 2 up to sqrt(max_number) (say variable i)
+	//cycle over j, and check i^2, i^2 + i, ... up to the closest to n (use a while maybe)
+	//for every element, put the boolean element to false
+
+	//print only the numbers for true elements
+
+	return 0;
+}
