7df37f7a8065c45b2a017506af602406a18a353a
larceci89 <larceci@sissa.it>
2017-11-12 16:52:35 +0100

    date.cc with helpers

--- exercises/03_classes/date.cc
+++ exercises/03_classes/date.cc
@@ -25,6 +25,17 @@ public:
 	Date add_oneday(Date date);
 };
 
+/*	logical operation to assess whether a year is leap or not
+	NOTE: it is defined before the function add_oneday, since this function
+	uses it	*/
+bool is_leap(const unsigned int y){
+	if (y % 4 == 0)
+		return true;
+	else
+		return false;
+}
+
+//	operation that adds one day at a time
 Date Date::add_oneday(Date date) {
 	unsigned int day = date.fday();
 	monthtype month = date.fmonth();
@@ -34,7 +45,10 @@ Date Date::add_oneday(Date date) {
 	switch (month)
 	{
 		case monthtype::feb:
-			day_threshold = 28;
+			if ( is_leap(year) )
+				day_threshold = 29;
+			else
+				day_threshold = 28;
 			break;
 		case monthtype::apr:
 		case monthtype::jun:
@@ -66,11 +80,38 @@ Date Date::add_oneday(Date date) {
 	return date;
 }
 
+//	logical equal operation between class elements
+bool operator == (const Date& lhs, const Date& rhs){
+	if ( lhs.fday() == rhs.fday() )
+	{
+	   if ( lhs.fmonth() == rhs.fmonth() )
+	   {
+		if ( lhs.fyear() == rhs.fyear() )
+			return true;
+	   }
+	}
+	return false;
+}
+
+bool operator != (const Date& lhs, const Date& rhs){
+	if (lhs == rhs)
+		return false;
+	else
+		return true;
+}
+
+//	operator that describes how to print a Date element with <<
+std::ostream& operator << (std::ostream& os, const Date& d){
+	return os << d.fday() << "/" << int(d.fmonth()) << "/" << d.fyear();
+}
+
+
 int main() {
 
 	unsigned int dd, mm, yy, numdays;
 	bool flag_stop = true;
 
+	// get the date in input
 	std::cout << "Insert the day: ";
 	std::cin >> dd;
 	std::cout << "\nInsert the month (number): ";
@@ -81,17 +122,28 @@ int main() {
 	
 	Date date(dd,mm,yy);
 
-	std::cout << "You have inserted the date: " << date.fday() << "/" << int(date.fmonth()) << "/" << date.fyear() << std::endl << std::endl;
+	// writes the input date
+	std::cout << "You have inserted the date: " << date << std::endl << std::endl;
 
+	// check and write if the year is leap or not
+	if ( is_leap(date.fyear()) )
+		std::cout << "This year is leap\n\n";
+	else
+		std::cout << "This year is not leap\n\n";
+
+	// ask for a number of days to add and compute the new date
+	// stops when 0 is entered
 	while(flag_stop){
-		std::cout << "Insert the number of days to add: ";
+		std::cout << "Insert the number of days to add (insert 0 to stop): ";
 		std::cin >> numdays;
-		date = date.add_day(date, numdays);
-		std::cout << "New date: " << date.fday() << "/" << int(date.fmonth()) << "/" << date.fyear() << std::endl << std::endl;
 		if(numdays == 0)
 			flag_stop = false;
-		std::cin.clear();
-		std::cin.ignore(1000, '\n');
+		else{
+			date = date.add_day(date, numdays);
+			std::cout << "New date: " << date.fday() << "/" << int(date.fmonth()) << "/" << date.fyear() << std::endl << std::endl;
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+		}
 	}
 
 	return 0;
